0f14648867099dce918fb122511ac2206dee8e8b
Javier Salido Gómez <javisago91@gmail.com>
2016-09-08 20:19:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ console.log("Server iniciado");
 vision.get('/',function(req,res){
 var pagina='<!doctype html><html><head></head><body>';   
     pagina+= '<form action=\"Registro\" method=\"post\">';
-    pagina += '<br><a href="/index.html">Puto Javi de mierda</a></br>';
+    pagina += '<br><a href="/index.html">Link</a></br>';
     //pagina += '<br><input type=\"submit\" value=\"Registrar\"></br>';
     
     pagina += '</form>';
